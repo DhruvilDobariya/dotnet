462689690bccd06ae40f8f7e6abd33a762eedaa2
Dhruvil Dobariya <190540107051@darshan.ac.in>
2022-11-24 11:44:15 +0530

    Complete Ch 25

---
+++ API Learning/code/Introduction to WEB Development/MVCLearn/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCLearn.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Employee Name")]
+        public string Name { get; set; } = null!;
+    }
+}
---
+++ API Learning/code/Introduction to WEB Development/WebAPILearn/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPILearn.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Employee Name")]
+        public string Name { get; set; } = null!;
+    }
+}
