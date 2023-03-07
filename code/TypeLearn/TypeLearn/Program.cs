2d6958f2111658e28862c85d5b424335314ff4be
DhruvilDobariya <190540107051@darshan.ac.in>
2023-03-07 10:26:07 +0530

    Learn difference  GetType and typeof

---
+++ code/TypeLearn/TypeLearn/Program.cs
@@ -0,0 +1,15 @@
+﻿using BookAPI.Domain.Models;
+
+namespace TypeLearn
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            User user = new User();
+
+            Console.WriteLine(user.GetType().ToString().Split(".")[3]);
+            Console.WriteLine(typeof(User).ToString().Split(".")[3]);
+        }
+    }
+}
---
+++ code/TypeLearn/TypeLearn/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookAPI.Domain.Models
+{
+    //[Index(nameof(Email), IsUnique = true)]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please eneter name")]
+        [MinLength(2, ErrorMessage = "Name must contains at least two character")]
+        public string Name { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please eneter email")]
+        [DataType(DataType.EmailAddress, ErrorMessage = "Please enter valid email")]
+        public string email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Please eneter password")]
+        [DataType(DataType.Password, ErrorMessage = "Please enter valid password")]
+        public string Password { get; set; } = string.Empty!;
+    }
+}
