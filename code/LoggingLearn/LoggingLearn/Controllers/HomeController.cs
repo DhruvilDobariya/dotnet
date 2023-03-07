1c6e519b270bf1485681c00b851211c7a01b02ae
DhruvilDobariya <190540107051@darshan.ac.in>
2023-03-07 10:08:03 +0530

    Learn LoggingLearn

---
+++ code/LoggingLearn/LoggingLearn/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoggingLearn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string RunAPI()
+        {
+            _logger.LogTrace("Log trace executed");
+            _logger.LogDebug("Log debug executed");
+            _logger.LogInformation("Log infromation executed");
+            _logger.LogWarning("Log warning executed");
+            _logger.LogError("Log Error executed");
+            _logger.LogCritical("Log critical executed");
+
+            _logger.Log(LogLevel.Error, "Log error executed by log");
+            _logger.LogInformation(_logger.IsEnabled(LogLevel.Information).ToString());
+
+            // Priority of logs,
+            // Trace < Debug < Information < Warning < Error < Critical < None
+            return "API run successfully";
+        }
+    }
+}
