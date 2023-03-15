a885e1e3a810125484689b14b8c0622bf3017f7e
DhruvilDobariya <190540107051@darshan.ac.in>
2023-03-15 12:52:35 +0530

    Benchmark learn

---
+++ code/BenchmarkLearn/BenchmarkLearn/Program.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace BenchmarkLearn
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Program>();
+            // benchmark method must non static and we must run program in release mode.
+        }
+
+        [Benchmark]
+        public void Method1(int n1)
+        {
+            for (int i = 0; i < n1; i++) { };
+        }
+
+        [Benchmark]
+        public void Method2(int n2)
+        {
+            for (int i = 0; i < n2; i++) { };
+        }
+    }
+}
