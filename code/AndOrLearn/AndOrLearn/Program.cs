49dfad0a1d502836022886bdcbe6b526b6af3603
dhruvil.d <190540107051@darshan.ac.in>
2023-04-28 16:12:40 +0530

    Learn AndOr

---
+++ code/AndOrLearn/AndOrLearn/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace AndOrLearn
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            OldSyntax(20);
+            NewSyntax(20);
+        }
+        public static void OldSyntax(int n)
+        {
+            if ((n >= 10 && n <= 20) || n == 21 || n == 4)
+            {
+                Console.WriteLine($"{n} is acceptable number");
+            }
+        }
+        public static void NewSyntax(int n)
+        {
+            // "and" and "or" is only used with "is".
+            if ((n is (>= 10 and <= 20)) || (n is (21 or 4)))
+            {
+                Console.WriteLine($"{n} is acceptable number");
+            }
+        }
+    }
+}
