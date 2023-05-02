49949e1546accdb65f57c88e4f4eac692ec11632
dhruvil.d <190540107051@darshan.ac.in>
2023-05-02 09:50:55 +0530

    Learn distributed cache using redis

---
+++ code/DistributedCaching/DistributedCaching/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace DistributedCaching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IDistributedCache _cache;
+        public HomeController(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            string token = string.Empty;
+            try
+            {
+                token = await _cache.GetStringAsync("token");
+                //Or
+                //token = Encoding.UTF8.GetString(await _cache.GetAsync("token"));
+            }
+            catch { }
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                return Ok(token);
+
+            }
+
+            token = GetTocken();
+            DistributedCacheEntryOptions options = new DistributedCacheEntryOptions()
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromSeconds(40),
+                // Or
+                //AbsoluteExpiration = DateTime.UtcNow.AddSeconds(40)
+            };
+            await _cache.SetStringAsync("token", token, options);
+            // Or
+            //byte[] tokenBytes = Encoding.UTF8.GetBytes(token);
+            //await _cache.SetStringAsync("token", tokenBytes, options);
+
+            return Ok(token);
+        }
+
+        private static string GetTocken()
+        {
+            string token = "Dhruvil";
+
+            Thread.Sleep(7000);
+
+            return token;
+        }
+    }
+}
---
+++ code/DistributedCaching/DistributedCaching/Models/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace DistributedCaching.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public int StudentName { get; set; }
+    }
+}
