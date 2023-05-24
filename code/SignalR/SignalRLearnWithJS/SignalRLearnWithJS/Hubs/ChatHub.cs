31d261c4ddac4b5d6150cb2c5a2ad4ab30460186
dhruvil.d <190540107051@darshan.ac.in>
2023-05-24 13:25:41 +0530

    Learn signalr with js

---
+++ code/SignalR/SignalRLearnWithJS/SignalRLearnWithJS/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRLearnWithJS.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string password)
+        {
+            await Clients.All.SendAsync("ReceivedMessage", user, password);
+        }
+    }
+}
