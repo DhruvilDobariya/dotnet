ed36e36cccba77a68a541f8e6861f227e7819da1
dhruvil.d <190540107051@darshan.ac.in>
2023-05-29 12:48:09 +0530

    Add APIGateWayLearn

--- code/SignalR/SignalRLearnWithJS/SignalRLearnWithJS/Hubs/ChatHub.cs
+++ code/SignalR/SignalRLearnWithJS/SignalRLearnWithJS/Hubs/ChatHub.cs
@@ -6,7 +6,7 @@ namespace SignalRLearnWithJS.Hubs
     {
         public async Task SendMessage(string user, string password)
         {
-            await Clients.All.SendAsync("ReceivedMessage", user, password);
+            await Clients.All.SendAsync( "ReceivedMessage", user, password);
         }
     }
 }
