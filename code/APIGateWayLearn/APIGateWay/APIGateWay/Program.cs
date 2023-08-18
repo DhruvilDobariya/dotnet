1f69392beb57d9c0fcdcf5c25fb54bbdbf39e23f
dhruvil.d <190540107051@darshan.ac.in>
2023-08-18 18:56:20 +0530

    Add Some comments

--- code/APIGateWayLearn/APIGateWay/APIGateWay/Program.cs
+++ code/APIGateWayLearn/APIGateWay/APIGateWay/Program.cs
@@ -10,3 +10,15 @@ await app.UseOcelot();
 app.MapControllers();
 
 app.Run();
+
+// Nuget: Ocelot
+// Folder Structure:
+// APIGateWay
+// => APIgateway Project
+//      => Program.cs
+//          => Main entry point of our micro-service application
+//      => Ocelot.json
+//          => Which contains which contains bridge configuration between services and gateway
+// => Service folder: which contains different services 
+//      => ProductService
+//      => UserService
