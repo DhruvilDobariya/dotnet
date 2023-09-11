9b7014f3d3b6fb802abb0e1ef21ea9ce3395000e
Dhruvil Dobariya <190540107051@darshan.ac.in>
2023-09-11 08:42:57 +0530

    Learn Collection literals

---
+++ code/CollectionLiteralLearn/CollectionLiteralLearn/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CollectionLiteralLearn
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<string> list = new List<string>() { "red", "blue", "green" };
+            List<string> newList = ["red", "blue", "green"];
+
+            Span<string> newSpan = ["purple", "lime", "cyan"];
+
+            List<string> colors = [.. newList, "pink", .. newSpan]; // merge collection
+            // colors = ["red", "blue", "green", "pink", "purple", "lime", "cyan"]
+
+            Dictionary<string, string> dict = new Dictionary<string, string>()
+            {
+                {"Name", "Dhruvil Dobariya" },
+                {"City", "Rajkot" },
+                {"State", "Gujarat" },
+                {"Country", "India" }
+            };
+
+            //Dictionary<string, string> newDict = [
+            //    "Name": "Dhruvil Dobariya", 
+            //    "City": "Rajkot", 
+            //    "State": "Gujarat", 
+            //    "Country": "India"
+            //];
+            // Currently not implemented but targeted for C#12
+        }
+    }
+}
